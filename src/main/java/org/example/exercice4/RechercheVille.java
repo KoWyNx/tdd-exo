362996a005a09535f5250eabb0e3ba4215b29294
KoWyNx <fberrani@hotmail.fr>
2024-10-17 11:14:22 +0200

    test 2

--- src/main/java/org/example/exercice4/RechercheVille.java
+++ src/main/java/org/example/exercice4/RechercheVille.java
@@ -1,11 +1,28 @@
 package org.example.exercice4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RechercheVille {
     private List<String> villes;
 
+    public RechercheVille() {
+        this.villes = new ArrayList<>(Arrays.asList(
+                "Paris", "Budapest", "Rotterdam",
+                "Valence", "Vancouver", "Amsterdam", "Vienne",
+                "Sydney"
+        ));
+    }
+
     public List<String> rechercher(String mot)  {
-        throw new NotImplementedException();
+        if (mot.length() < 2 && !mot.equals("*")) {
+            throw new NotImplementedException("Le texte de recherche doit contenir au moins 2 caractères.");
+        }
+        return villes.stream()
+                .filter(ville -> ville.startsWith(mot))
+                .collect(Collectors.toList());
+
     }
 }
