997be5bdc7434e45b1ccac17355712b5da1d34b6
KoWyNx <fberrani@hotmail.fr>
2024-10-17 12:22:01 +0200

    fin exercice 5

--- src/main/java/org/example/exercice5/Product.java
+++ src/main/java/org/example/exercice5/Product.java
@@ -1,5 +1,40 @@
 package org.example.exercice5;
 
 public class Product {
+    private String type;
+    private String name;
+    private int sellIn;
+    private int quality;
 
+    public Product(String type, String name, int sellIn, int quality) {
+        this.type = type;
+        this.name = name;
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public void setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
+        this.quality = quality;
+    }
 }
--- src/main/java/org/example/exercice5/Shop.java
+++ src/main/java/org/example/exercice5/Shop.java
@@ -1,7 +1,37 @@
 package org.example.exercice5;
 
-
+import java.util.List;
 
 public class Shop {
+    private List<Product> products;
 
+    public Shop(List<Product> products) {
+        this.products = products;
+    }
+
+    public void update(Product product) {
+        product.setSellIn(product.getSellIn() - 1);
+
+        int qualityChange = product.getSellIn() < 0 ? 2 : 1;
+
+        if (product.getType().equals("brie")) {
+            if (product.getQuality() < 50) {
+                product.setQuality(Math.min(50, product.getQuality() + qualityChange));
+            }
+        } else if (product.getType().equals("laitier")) {
+            if (product.getQuality() > 0) {
+                product.setQuality(Math.max(0, product.getQuality() - qualityChange * 2));
+            }
+        } else {
+            if (product.getQuality() > 0) {
+                if (product.getQuality() < 50) {
+                    product.setQuality(Math.max(0, product.getQuality() - qualityChange));
+                }
+            }
+        }
+
+        if (product.getQuality() > 50) {
+            product.setQuality(50);
+        }
+    }
 }
