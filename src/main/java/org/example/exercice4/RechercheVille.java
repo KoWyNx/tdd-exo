14b91f29eb37e251a62741c972dff2940f92885b
KoWyNx <fberrani@hotmail.fr>
2024-10-17 11:02:44 +0200

    fin du premier test

--- src/main/java/org/example/exercice4/RechercheVille.java
+++ src/main/java/org/example/exercice4/RechercheVille.java
@@ -3,5 +3,9 @@ package org.example.exercice4;
 import java.util.List;
 
 public class RechercheVille {
+    private List<String> villes;
 
+    public List<String> rechercher(String mot)  {
+        throw new NotImplementedException();
+    }
 }
